09bc64447dcbf2f364697d61ecb9124c483550ee
MindruAndrei <andrei.mindru@fullscreendigital.ro>
2023-05-07 23:05:49 +0300

    feat: timkeepings create form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,60 +1,61 @@
-import './App.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { ListItem, List } from '@mui/material';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ListItem, List } from "@mui/material";
+import TimekeepingCreateUpdateForm from "./components/TimekeepingCreateForm";
 
-const create = () => {
-  return axios.post('http://localhost:3001/api/timekeeping', {
-    description: "debugging",
-    date: new Date(),
-    time: 8
-  })
-}
+const create = (payload) => {
+  return axios.post("http://localhost:3001/api/timekeeping", payload);
+};
 
 const getEntries = () => {
-  return axios.get('http://localhost:3001/api/timekeeping')
-}
+  return axios.get("http://localhost:3001/api/timekeeping");
+};
 
 function App() {
   const [entries, setEntries] = useState([]);
+  const [loadData, setLoadData] = useState([]);
 
   useEffect(() => {
-    getEntries().then(response => setEntries(response.data.data))
-  }, [])
+    getEntries().then((response) => setEntries(response.data.data));
 
-  console.log(entries)
+    if(loadData){
+      setLoadData(false)
+    }
+  }, [loadData]);
+
   return (
     <div className="App">
-      <h1 className='text-2xl mt-10'>Pontaje</h1>
-      <List >
+      <h1 className="mb-4 text-3xl">
+        Timekeepings
+      </h1>
+      <List>
         <ListItem>
-          <div className='w-[33%] flex justify-center font-bold'>
-            Data
-          </div>
-          <div className='w-[33%] flex justify-center font-bold'>
-            Descriere
-          </div>
-          <div className='w-[33%] flex justify-center font-bold'>
-            Pontaj(h)
-          </div>
+          <div className="w-[33%] flex justify-center font-bold">Date</div>
+          <div className="w-[33%] flex justify-center font-bold">Description</div>
+          <div className="w-[33%] flex justify-center font-bold">Time(hours)</div>
         </ListItem>
-        {
-          entries.map(entry => {
-            return <ListItem>
-              <div className='w-[33%] flex justify-center mt-2'>
+        {entries.map((entry) => {
+          return (
+            <ListItem>
+              <div className="w-[33%] flex justify-center mt-2">
                 {entry.date}
-              </div >
-              <div className='w-[33%] flex justify-center mt-2'>
+              </div>
+              <div className="w-[33%] flex justify-center mt-2">
                 {entry.description}
               </div>
-              <div className='w-[33%] flex justify-center mt-2'>
+              <div className="w-[33%] flex justify-center mt-2">
                 {entry.time}
               </div>
             </ListItem>
-          })
-        }
+          );
+        })}
       </List>
-      <button className='mt-10' onClick={create}>Req</button>
+      <TimekeepingCreateUpdateForm
+        setLoadData={setLoadData}
+        onSubmit={create}
+        initialValues={{}}
+      />
     </div>
   );
 }
